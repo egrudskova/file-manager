5d14484c7b8398d3d3a6fd7db55036189e5833fa
Elina <elina50505051@yandex.ru>
2024-10-15 01:10:53 +0300

    feat: add hash command

--- src/commands/hashCommandsHandler.js
+++ src/commands/hashCommandsHandler.js
@@ -1,3 +1,21 @@
-export const hashCommandsHandler = () => {
+import path from "node:path";
+import {createReadStream} from "node:fs";
+import {createHash} from "node:crypto";
 
+export const hashCommandsHandler = (...args) => {
+  if (args.length !== 1) {
+    throw new Error('Invalid input');
+  }
+  try {
+    const [fileName] = args;
+    const filePath = path.resolve(process.cwd(), fileName);
+    const readStream = createReadStream(filePath);
+    const hash = createHash('sha256');
+
+    readStream.pipe(hash).on('finish', () => {
+      console.log(hash.digest("hex"));
+    })
+  } catch {
+    throw new Error('Operation failed');
+  }
 }
